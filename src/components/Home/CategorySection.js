7c6ae947c943a4e50931c879bb4aefe00d5ef930
Peter Olu <pemeraldy@gmail.com>
2023-02-15 04:55:00 +0100

    modified course category section

--- src/components/Home/CategorySection.js
+++ src/components/Home/CategorySection.js
@@ -18,7 +18,7 @@ class Category extends Component {
                         </div>
                         <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-4">
                             <div className="category__more mb-50 float-md-end fix">
-                                <Link href="/course-grid"><a className="link-btn">
+                                <Link href="/"><a className="link-btn">
                                     View all Category
                                     <i className="fas fa-arrow-right"></i>
                                     <i className="fas fa-arrow-right"></i>
@@ -39,8 +39,8 @@ class Category extends Component {
                                     </svg>
                                 </div>
                                 <div className="category__content">
-                                <h4 className="category__title"><Link href="/course-details"><a>Data Science</a></Link></h4>
-                                <p>Data is Everything</p>
+                                <h4 className="category__title"><Link href="/course-details"><a>French</a></Link></h4>
+                                <p>Speak french in just weeks</p>
                                 </div>
                             </div>
                         </div>
@@ -61,7 +61,7 @@ class Category extends Component {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                        {/* <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6">
                             <div className="category__item mb-30 transition-3 d-flex align-items-center">
                                 <div className="category__icon mr-30">
                                 <svg viewBox="0 0 512 512">
@@ -77,8 +77,8 @@ class Category extends Component {
                                 <p>Fun & Challenging</p>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                        </div> */}
+                        {/* <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6">
                             <div className="category__item mb-30 transition-3 d-flex align-items-center">
                                 <div className="category__icon mr-30">
                                 <svg viewBox="0 0 512 512">
@@ -94,7 +94,7 @@ class Category extends Component {
                                 <p>New Skills, New You</p>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                         <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6">
                             <div className="category__item mb-30 transition-3 d-flex align-items-center">
                                 <div className="category__icon mr-30">
@@ -115,16 +115,17 @@ class Category extends Component {
                         <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6">
                             <div className="category__item mb-30 transition-3 d-flex align-items-center">
                                 <div className="category__icon mr-30">
-                                <svg viewBox="0 0 512 512">
-                                    <g>
-                                        <path className="st4" d="M438,512c-40.8,0-74-33.2-74-74V74c0-40.8,33.2-74,74-74s74,33.2,74,74v364C512,478.8,478.8,512,438,512z    M438,40c-18.7,0-34,15.3-34,34v364c0,18.7,15.3,34,34,34s34-15.3,34-34V74C472,55.3,456.7,40,438,40z M74,512   c-40.8,0-74-33.2-74-74v-82c0-40.8,33.2-74,74-74s74,33.2,74,74v82C148,478.8,114.8,512,74,512z M74,322c-18.7,0-34,15.3-34,34v82   c0,18.7,15.3,34,34,34s34-15.3,34-34v-82C108,337.3,92.7,322,74,322z M256,512c-40.8,0-74-33.2-74-74V213c0-40.8,33.2-74,74-74   s74,33.2,74,74v225C330,478.8,296.8,512,256,512z M256,179c-18.7,0-34,15.3-34,34v225c0,18.7,15.3,34,34,34s34-15.3,34-34V213   C290,194.3,274.7,179,256,179z"/>
-                                        <path className="st5" d="M139,162.3c0-31.2-27.8-56.7-61.9-56.7c-14.5,0-25.1-7-25.1-16.7c0-9.2,7.4-16.7,16.5-16.7   c9.2,0,21.1,1,40.2,8.4c10.3,4,21.9-1.1,25.9-11.4s-1.1-21.9-11.4-25.9c-9.9-3.9-18.6-6.4-26.2-8.1V20C97,9,88,0,77,0S57,9,57,20   v13.5C31.3,38.9,12,61.7,12,89c0,32.3,28,56.7,65.1,56.7c11.9,0,21.9,7.6,21.9,16.7c0,8.9-8.3,16.7-17.7,16.7   c-7.3,0-25.8-2.7-43.9-9.9c-10.3-4.1-21.9,0.9-26,11.2s0.9,21.9,11.2,26c11.6,4.6,23.7,7.9,34.4,10V228c0,11,9,20,20,20s20-9,20-20   v-11.2c9.1-2.6,17.6-7.4,24.6-14.2C132.8,191.9,139,177.6,139,162.3L139,162.3z"/>
-                                    </g>
+                                    <svg viewBox="0 0 512 512">
+                                        <g>
+                                            <path className="st0" d="M178.7,492H120c-55.2,0-100-44.8-100-100V120C20,64.8,64.8,20,120,20h58.7C123.7,20,81,64.8,81,120v272   C81,447.2,123.7,492,178.7,492z M355.5,204.8l18.9-85.5c4.8-24.1,16.7-46.3,34.1-63.7l35.4-35.4c-15.1-1.4-30.7,3.7-42.3,15.3   l-61.1,61.1c-17.4,17.4-29.3,39.6-34.1,63.7L295,217l56.7-11.3C352.9,205.4,354.2,205.1,355.5,204.8L355.5,204.8z"/>
+                                            <path className="st1" d="M299,512H120C53.8,512,0,458.2,0,392V120C0,53.8,53.8,0,120,0h183c11,0,20,9,20,20s-9,20-20,20H120   c-44.1,0-80,35.9-80,80v272c0,44.1,35.9,80,80,80h179c44.1,0,80-35.9,80-80V272c0-11,9-20,20-20s20,9,20,20v120   C419,458.2,365.2,512,299,512z M298.9,236.6l56.7-11.3c28.1-5.6,53.7-19.3,73.9-39.6l61.1-61.1c28.5-28.5,28.5-74.8,0-103.2   c-28.5-28.5-74.8-28.5-103.2,0l-61.1,61.1c-20.3,20.3-33.9,45.8-39.6,73.9l-11.3,56.7c-1.3,6.6,0.7,13.3,5.5,18.1   c3.8,3.8,8.9,5.9,14.1,5.9C296.3,237,297.6,236.9,298.9,236.6L298.9,236.6z M462.4,49.7c6.2,6.2,9.7,14.5,9.7,23.3   s-3.4,17.1-9.7,23.3l-61.1,61.1c-14.7,14.7-33.2,24.6-53.5,28.6l-27.3,5.4l5.4-27.3c4.1-20.3,14-38.8,28.6-53.5l61.1-61.1   c6.2-6.2,14.5-9.7,23.3-9.7S456.1,43.4,462.4,49.7L462.4,49.7z"/>
+                                            <path className="st2" d="M319,352H101c-11,0-20-9-20-20s9-20,20-20h218c11,0,20,9,20,20S330.1,352,319,352z M211,387   c-13.8,0-25,11.2-25,25s11.2,25,25,25s25-11.2,25-25S224.8,387,211,387z"/>
+                                        </g>
                                     </svg>
                                 </div>
                                 <div className="category__content">
-                                <h4 className="category__title"><Link href="/course-details"><a>Finance</a></Link></h4>
-                                <p>Fun & Challenging</p>
+                                <h4 className="category__title"><Link href="/course-details"><a>IELTS</a></Link></h4>
+                                <p>Get certified in weeks</p>
                                 </div>
                             </div>
                         </div>
@@ -146,7 +147,7 @@ class Category extends Component {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                        {/* <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6">
                             <div className="category__item mb-30 transition-3 d-flex align-items-center">
                                 <div className="category__icon mr-30">
                                 <svg viewBox="0 0 512 512">
@@ -165,7 +166,7 @@ class Category extends Component {
                                 <p>Major or Minor</p>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                         <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6">
                             <div className="category__item mb-30 transition-3 d-flex align-items-center">
                                 <div className="category__icon mr-30">
