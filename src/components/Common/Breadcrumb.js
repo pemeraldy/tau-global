f560620b99eca526c7caed0ad1fea64483050427
Peter Olu <pemeraldy@gmail.com>
2023-02-21 11:35:14 +0100

    update social handles

--- src/components/Common/Breadcrumb.js
+++ src/components/Common/Breadcrumb.js
@@ -9,12 +9,12 @@ const Breadcrumb = (props) => {
                <div className="row">
                   <div className="col-xxl-12">
                      <div className="page__title-wrapper mt-110">
-                        <h3 className="page__title">{ pageTitle ? pageTitle : 'Courses'}</h3>                         
+                        {/* <h3 className="page__title">{ pageTitle ? pageTitle : 'Courses'}</h3>                          */}
                         <nav aria-label="breadcrumb">
-                           <ol className="breadcrumb">
+                           {/* <ol className="breadcrumb">
                               <li className="breadcrumb-item"><Link href="/" as="/"><a>Home</a></Link></li>
                               <li className="breadcrumb-item active" aria-current="page">{ pageTitle ? pageTitle : 'Courses'}</li>
-                           </ol>
+                           </ol> */}
                         </nav>
                      </div>
                   </div>
